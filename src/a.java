1b0dafe02f7b877f6aa80a80b60ef23cc7e86d90
hai <fifahayrataolahai1762004@gmail.com>
2024-04-26 18:24:59 +0700

    hello hai

--- src/a.java
+++ src/a.java
@@ -1,4 +1,43 @@
 public class a {
 private String a;
 
+public a(){
+    System.out.printf("sdad");
+
+}
+
+    public String getA() {
+        return a;
+    }
+
+    public void setA(String a) {
+        this.a = a;
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+
+
+
+
+
+
+
 }
