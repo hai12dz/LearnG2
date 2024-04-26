b6014f57403245d45e11f29b63639228415b5420
hai <fifahayrataolahai1762004@gmail.com>
2024-04-27 05:06:59 +0700

    heelo hai dz

--- src/abcd.java
+++ src/abcd.java
@@ -1,2 +1,30 @@
 public class abcd {
+    private String a;
+    private String b;
+    private int v;
+
+    public String getA() {
+        return a;
+    }
+
+    public void setA(String a) {
+        this.a = a;
+    }
+
+    public String getB() {
+        return b;
+    }
+
+    public void setB(String b) {
+        this.b = b;
+    }
+
+    public int getV() {
+        return v;
+    }
+
+    public void setV(int v) {
+        this.v = v;
+    }
+
 }
