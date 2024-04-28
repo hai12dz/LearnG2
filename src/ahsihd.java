1a7990883867162f858bd4962f736e675db25a27
hai <fifahayrataolahai1762004@gmail.com>
2024-04-28 22:04:16 +0700

    a

---
+++ src/ahsihd.java
@@ -0,0 +1,25 @@
+public class ahsihd {
+    private String a;
+    public int abc;
+
+    public ahsihd(String a, int abc) {
+        this.a = a;
+        this.abc = abc;
+    }
+
+    public String getA() {
+        return a;
+    }
+
+    public void setA(String a) {
+        this.a = a;
+    }
+
+    public int getAbc() {
+        return abc;
+    }
+
+    public void setAbc(int abc) {
+        this.abc = abc;
+    }
+}
---
+++ src/sbj.java
@@ -0,0 +1,15 @@
+public class subj {
+    public String dateToString;
+
+    public String getDateToString() {
+        return dateToString;
+    }
+
+    public void setDateToString(String dateToString) {
+        this.dateToString = dateToString;
+    }
+
+    public subj(String dateToString) {
+        this.dateToString = dateToString;
+    }
+}
